60f13f1e5d16d25c2aabd02db214be3569831d7d
Vitor Machado <vitimachado@hotmail.com>
2024-05-23 11:40:13 +0100

    Create the modal

--- src/components/VideoTrailer/index.tsx
+++ src/components/VideoTrailer/index.tsx
@@ -1,9 +1,21 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '../../data/store'
 import { Player } from '../../types/common'
 import { PlayerComponent } from './playerComponent';
+import { Modal } from '../Modal';
+import { clearVideoKey } from '../../data/movieSlice';
 
 export const VideoTrailer = ({ player = 'youtube' }: Player) => {
+    const dispatch = useDispatch();
     const videoKey = useSelector<RootState>((state) => state?.movie?.videoKey) as string | null;
-    return <PlayerComponent videoKey={videoKey} player={player} />
+
+    const handleOnClose = () => {
+        dispatch(clearVideoKey())
+    }
+
+    return (
+        <Modal open={!!videoKey} onClosed={handleOnClose}>
+            <PlayerComponent videoKey={videoKey} player={player} />
+        </Modal>
+    )
 }
--- src/types/common.ts
+++ src/types/common.ts
@@ -1,6 +1,17 @@
+import { ReactNode } from "react";
+
 export type FetchStatus = '' | 'success' | 'loading' | 'error';
 
 export interface Player {
     player?: string
     videoKey?: string | null
+}
+
+export interface IModal {
+    children: ReactNode
+    title?: string
+    hasDivider?: boolean
+    open?: boolean
+    closeOnClickOutside?: boolean
+    onClosed?: () => void
 }
